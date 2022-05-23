282fe33768c33aa4bda09623df7b87ad9e3ef011
LeeRJennings <leerjennings@outlook.com>
2022-05-23 13:21:39 -0500

    phase 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
+int secretNumber = 42;
+
 Console.WriteLine("Guess a number");
 string userGuess = Console.ReadLine();
-Console.WriteLine(userGuess);
+
+int usersGuessParsed = int.Parse(userGuess);
+
+if (usersGuessParsed == secretNumber) 
+{
+    Console.WriteLine("You got it!");
+}
+else 
+{
+    Console.WriteLine("Nah dude, try again.");
+}
