6a62953e359b7ee0b873dd390849eae788256311
LeeRJennings <leerjennings@outlook.com>
2022-05-23 13:38:25 -0500

    phase 3

--- Program.cs
+++ Program.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 
 int secretNumber = 42;
 
-Console.WriteLine("Guess a number");
-string userGuess = Console.ReadLine();
-
-int usersGuessParsed = int.Parse(userGuess);
-
-if (usersGuessParsed == secretNumber) 
+int guessFunc() 
 {
-    Console.WriteLine("You got it!");
+    Console.WriteLine("Guess a number");
+    string userGuess = Console.ReadLine();
+    int usersGuessParsed = int.Parse(userGuess);
+    return usersGuessParsed;
 }
-else 
+
+int guess = guessFunc();
+int count = 1;
+
+while(count < 4)
 {
-    Console.WriteLine("Nah dude, try again.");
+    if (guess == secretNumber) 
+    {
+        Console.WriteLine("You got it!");
+        break;
+    }
+    else 
+    {
+        Console.WriteLine("Nah dude, try again.");
+        count++;
+    }
+    guess = guessFunc();
 }
